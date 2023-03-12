e799818ba2767f5c14e07b96bc510f52e76cd872
Edwin Yu <edwinyyyu@gmail.com>
2023-03-11 16:36:54 -0800

    Implement button behavior

--- piano.v
+++ piano.v
@@ -50,39 +50,66 @@ module piano
     .inc_octave(inc_octave), .dec_octave(dec_octave)
   );
 
-  wire add;
-  reg [5:0] next_note;
-  wire pb_note;
+  reg [2:0] octave;
+  reg [2:0] octave_prev;
+  reg [2:0] note_prev;
 
-  note_buffer recording(.clk(clk), .rst(rst),
-    .add(add), .next_note(next_note), .pb_note(pb_note)
-  );
+  reg [5:0] buffer [255:0];
+  reg [7:0] idx;
 
-  reg pb_mode;
-  reg pb_next_mode;
+  reg [6:0] note_switches_prev;
+  reg toggle_pb_prev;
+  reg inc_octave_prev;
+  reg dec_octave_prev;
 
-  always @ (posedge toggle_pb) begin
-    pb_next_mode <= !pb_mode;
-  end
-
-  always @ (posedge ^note_switches or posedge ~^note_switches) begin
-    if (!pb_mode) begin
-      // PLAY NOTE & SAVE NOTE
-    end
-  end
-
-  reg [7:0] pb_state;
+  integer i;
   always @ (posedge clk) begin
     if (rst) begin
+      // Clear recording
+      for (i = 0; i < 256; i = i + 1) begin
+        buffer[i] <= 6'b0;
+      end
+      idx <= 8'b0;
+
+      // Reset currently selected octave, last note and octave
+      octave <= 3'd4;
+      octave_prev <= 3'd0;
+      note_prev <= 3'd0;
+
+      // Reset to recording mode
       pb_mode <= 1'b0;
-      // RESET OCTAVE
     end else begin
-      pb_mode <= pb_next_mode;
+      // Update currently selected octave
+      if (octave < 3'd7 && inc_octave && !inc_octave_prev) begin
+        octave = octave + 1;
+      end
+      if (octave > 3'd1 && dec_octave && !dec_octave_prev) begin
+        octave = octave - 1;
+      end
+
+      // Toggle mode
+      if (toggle_pb && !toggle_pb_prev) begin
+        pb_mode = !pb_mode;
+        // TODO: DO CLEANUP IF NECESSARY
+      end
+
       if (pb_mode) begin
-        // DO STUFF
+        // IF IN PLAYBACK MODE
+        // TODO: DO STUFF
+        // TODO: RESET pb_mode to 0 WHEN DONE PLAYING
+      end else if (note_switches != note_switches_prev) begin
+        // Switches have changed
+        // TODO: Play note
+        // TODO: Save note
       end else begin
-        // DO STUFF
+        // Switches have not changed, not in pb_mode
+        // TODO: Maybe do stuff here
       end
     end
+
+    note_switches_prev <= note_switches;
+    toggle_pb_prev <= toggle_pb;
+    inc_octave_prev <= inc_octave;
+    dec_octave_prev <= dec_octave;
   end
 endmodule
